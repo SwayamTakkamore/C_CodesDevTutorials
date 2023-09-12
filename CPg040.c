fb2ee224182fb2ae4a0a1075cf29c25ce604ab46
Swayam Takkamore <70209059+SwayamTakkamore@users.noreply.github.com>
2023-09-11 22:22:19 -0700

    Update CPg040.c

--- CPg040.c
+++ CPg040.c
@@ -1,3 +1,25 @@
 // To Check whether entered no. is a Prime no. or not
 
-
+#include <stdio.h>
+void main()
+{
+    int i, num, count;
+    printf("Enter any number : ");
+    scanf("%d", &num);
+    for(i=2;i<=num;i++)
+    {
+        if(num%i==0)
+        {
+            count++;
+        }
+    }
+    if(count>=2)
+    {
+        printf("%d is a Composite no.",num);
+    }
+    else
+    {
+        printf("%d is a Prime no.",num);
+    }
+    
+}
