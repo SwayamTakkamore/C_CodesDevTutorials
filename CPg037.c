e3c3fcbb2c5f391a69f6a184a9dbf82e803cac81
Swayam Takkamore <70209059+SwayamTakkamore@users.noreply.github.com>
2023-09-11 22:27:31 -0700

    Update CPg037.c

--- CPg037.c
+++ CPg037.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+void main() 
+{
+  int i, num;
+  printf("Enter any Value : ");
+  scanf("%d",&num);
+  for(i=1;i<=10;i++)
+    {
+      printf(\n %d x %d = %d",num,i,num*i);
+    }
+}
