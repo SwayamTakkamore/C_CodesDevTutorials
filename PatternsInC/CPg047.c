abdf0b29b4c1c74f061fd83e2785a36ca7503a87
Swayam Takkamore <70209059+SwayamTakkamore@users.noreply.github.com>
2023-09-25 10:16:49 +0530

    Create CPg047.c

---
+++ PatternsInC/CPg047.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() 
+{
+    int i, j;
+    for(i=65;i<=69;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+        printf("%c ", i);    
+        }
+        printf("\n");
+    }
+}
